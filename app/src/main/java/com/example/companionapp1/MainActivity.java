9da885655f5b1430dfc206c9b4545a6f2a4c47c3
AndrzejChmielewski <89131357+AndrzejChmielewski@users.noreply.github.com>
2023-09-22 13:03:34 +0200

    2
    1

--- app/src/main/java/com/example/companionapp1/MainActivity.java
+++ app/src/main/java/com/example/companionapp1/MainActivity.java
@@ -14,22 +14,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Find the button by its ID
+        // Find the "Installation Guide" button by its ID
+        Button guideButton = findViewById(R.id.guideButton);
+
+        // Set a click listener for the "Installation Guide" button
+        guideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Define the URL of your installation guide and create an Intent to open it
+                String guideUrl = "https://your-installation-guide-url.com"; // Replace with your installation guide URL
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(guideUrl));
+
+                // Start the activity to open the installation guide link
+                startActivity(intent);
+            }
+        });
+
+        // Find the "Install Now" button by its ID
         Button storeButton = findViewById(R.id.storeButton);
 
-        // Set a click listener for the button
+        // Set a click listener for the "Install Now" button
         storeButton.setOnClickListener(new View.OnClickListener() {
             @Override
-
-
             public void onClick(View v) {
                 // Define the URL of your store and create an Intent to open it
-                String storeUrl = "https://play.google.com/store/apps/dev?id=4887226526184134400"; // Replace with your store URL
+                String storeUrl = "https://docs.google.com/document/d/e/2PACX-1vQZheo56qNTpMBpS3zoCIuq3EhaZRPM2Y0JXf2OqJD8YqYt1kZk8bArWVtSW0qumt6b57zShJUxwu9a/pub"; // Replace with your store URL
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(storeUrl));
 
                 // Start the activity to open the store link
                 startActivity(intent);
-
             }
         });
     }
